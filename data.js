6158de936d9b872cb709cdb5474e518a14ab736c
Rafaby <fabriceheriniaina74@gmail.com>
2024-06-17 21:50:56 +0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,10 @@
+var sensorData = {
+    temperature1: 0,
+    temperature2: 0,
+    temperature3: 0,
+    temperature4: 0,
+    relay1: false,
+    relay2: false,
+    relay3: false,
+    relay4: false
+  };
